44626f35a165103a53360f93f0ed4d75b2c0b2b8
inknsharps <nchen25@gmail.com>
2021-02-19 01:14:57 -0500

    add word guessing game init files

---
+++ 02_feb_2021/02_18_21/assets/script.js
@@ -0,0 +1,71 @@
+// Word guessing generation
+// Declare wordlist (array), selected word (array), blank version of selected word (string), the letters of the selected word (array)
+var wordList = ["banana", "apple", "pear"];
+var wordSelected;
+var wordSelectedAnswer;
+var wordToMatch;
+
+// Selects the word field element for displaying answer
+var wordFieldEl = document.querySelector(".word-field");
+
+// Guessing logic
+// Declare letter to be guessed, and guessed letters array
+var letter;
+var guessedLetters = [];
+// When a key is pressed, convert key to uppercase, add the key pressed to the guessedLetters array, add key to elements on the page, run checkLetters function
+document.onkeypress = function(event) {
+    var guessedLettersEl = document.querySelector(".guessed-letters");
+    letter = event.key.toUpperCase();
+    guessedLetters.push(letter);
+    guessedLettersEl.textContent = guessedLetters;
+    checkLetters(letter);
+}
+
+// Function to check guessed typed in letters, and add them to the word field if they match
+function checkLetters(a){
+    // For loop to iterate through the wordToMatch array (remember that this is currently an array of letters), and if the letter put through the function matches, splice them into  wordSelectedAnswer (which has the blanks)
+    for (var i = 0; i < wordToMatch.length; i++){
+        if (a === wordToMatch[i]){
+            wordSelectedAnswer.splice(i, 1, a);
+        }
+        // When the letters are guessed and match, add them to the wordSelectedAnswer array
+        wordFieldEl.textContent = wordSelectedAnswer.join("");
+    }
+}
+
+// Build section for the word to guess 
+function buildWordGuess(){
+    // Get a random word from the wordList array and turn it into uppercase
+    wordSelected = wordList[Math.floor(Math.random() * wordList.length)].toUpperCase();
+    console.log("The selected word is:", wordSelected);
+    // Converts wordSelected (which is currently an array w/ 1 index), into a string first, so a split method can be applied, and the individual letters of that word are put into an array
+    wordToMatch = wordSelected.toString().split("");
+    // Build the empty field for the word using " _ "
+    wordSelectedAnswer = [];
+    for (var i = 0; i < wordSelected.length; i++){
+        wordSelectedAnswer.push(" _ ");
+    }
+    wordFieldEl.textContent = wordSelectedAnswer.join("");
+}
+
+// Function to run a timer
+var gameTimerEl = document.querySelector(".timer");
+function gameTimer(){
+    var timeRemaining = 10;
+    var timeInterval = setInterval(() => {
+        if (timeRemaining !== 0){
+        gameTimerEl.textContent = timeRemaining;
+        timeRemaining--;
+        } else {
+        clearInterval(timeInterval);
+        gameTimerEl.textContent = "Time's up!";
+        }
+    }, 1000);   
+}
+
+
+// Start the game with a button functionality
+// Event listener for button, which runs gameTimer function
+var startButton = document.querySelector("#game-start");
+startButton.addEventListener("click", gameTimer);
+startButton.addEventListener("click", buildWordGuess);
---
+++ 02_feb_2021/02_18_21/readme.md
@@ -0,0 +1,18 @@
+# Word Guessing Game
+
+This is an early attempt at the word guessing game that was an activity for the Web API unit at the Columbia Engineering BCS Bootcamp.
+
+## Current Coded Functionality 
+
+* Timer functionality
+* Record keypresses functionality
+* Randomly generate a word from an array
+* Turn that randomly generated word into blanks (which is an array)
+* Check inputted keypresses against the randomly generated word
+* If inputted keypresses match, splice those letters into the blanks array
+
+## To-do
+
+* Win and lose conditions and notifications
+* Limit inputs to only letters
+* Persistent win loss records
