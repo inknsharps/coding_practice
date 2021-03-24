90229b1ca475fc1bd0c53cb6b593119999dea520
inknsharps <nchen25@gmail.com>
2021-03-24 12:14:54 -0400

    add some extra notes about inquirer's objects

--- 03_mar_2021/03_24_21/inquirer_practice_2/index.js
+++ 03_mar_2021/03_24_21/inquirer_practice_2/index.js
@@ -1,16 +1,44 @@
+const fs = require("fs");
 const inquirer = require('inquirer');
 const questions = [
     {
         type: "input",
         name: "first_name",
-        message: "What is your first name?"
+        message: "What is your first name?",
+        validate: function(answer){
+            if (!answer){
+                console.log("Required input not received. Aborting...");
+                process.exit(1);
+            }
+            return true;
+        },
     },
     {
         type: "input",
         name: "favourite_food",
-        message: "What is your favourite food?"
+        message: "What is your favourite food?",
+        validate: function(answer){
+            if (!answer){
+                console.log("Required input not received. Aborting...");
+                process.exit(1);
+            }
+            return true;
+        },
+        filter: function(value){
+            return value.toLowerCase();
+        }
+    },
+    {
+        type: "list",
+        name: "order_frequency",
+        message: "How often do you eat your favourite food?",
+        choices: ["Not very often", "Occassionally", "Very often"],
+        filter: function(value){
+            return value.toLowerCase();
+        }
     }
 ]
+
 // Regular promise based inquirer function
 // function askName(){
 //     inquirer
@@ -33,7 +61,11 @@ const questions = [
 async function askName(){
     let answers = await inquirer.prompt(questions);
     console.log(answers)
-    console.log(`My name is ${answers.first_name} and my favourite food is ${answers.favourite_food}!`);
+    let answersLog = `My name is ${answers.first_name} and my favourite food is ${answers.favourite_food}. I have it ${answers.order_frequency}!`;
+    fs.writeFile("myfavouritefoods.md", answersLog, err => {
+        if (err) throw err;
+        console.log("File created!");
+    });
 }
 
 askName();
--- 03_mar_2021/03_24_21/inquirer_practice_2/readme.md
+++ 03_mar_2021/03_24_21/inquirer_practice_2/readme.md
@@ -3,3 +3,6 @@
 Some key takeaways from this practice:
 * `.prompt()` in inquirer returns a promise, which is why the `.then()` and `.catch()` methods are used.
 * Because `.prompt()` returns a promise, you can apply async/await to really trim down the code.
+* The question object basically determines how the prompts behave, so remember to include the ones you need for best functionality: [Documentation](https://www.npmjs.com/package/inquirer#objects)
+    * For exmaple, if you want to check for a specific answer, make sure to include `validate` and use a function to check for any specific thing. In the example I put a basic "if there is nothing inputted, end the process" function (fun note about `validate`- you need to `return true` at the end of it, otherwise it will get stuck there).
+* `process.exit(integer)` is a way to abort the current process. In terms of the integer, `0` is a successful exit, while `1` is failed exit.
